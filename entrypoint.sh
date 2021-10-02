f6d619c5a834a81e97320ac73c81cccf0c043f15
CFE <hello@teamcfe.com>
2021-10-02 14:35:31 -0500

    Updated entrypoint for gunicorn worker tmp dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8000}
 
-/opt/venv/bin/gunicorn app.main:app -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}"
+/opt/venv/bin/gunicorn app.main:app --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}"
