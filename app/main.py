bfcb1260b986cc50153c61725c9ff7d7c2d3c497
CFE <hello@teamcfe.com>
2021-10-02 14:39:51 -0500

    Updated models

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 MODEL_DIR = BASE_DIR.parent / 'models'
-MODEL_PATH = MODEL_DIR / "exports/spam-sms/spam-model.h5"
+MODEL_PATH = MODEL_DIR / "spam-sms/spam-model.h5"
 
 @app.get("/")
 def read_index():
