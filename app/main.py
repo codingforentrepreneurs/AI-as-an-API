1e6a5fc76e9c819bd0a93d6a5b8509c9b2ec3238
CFE <hello@teamcfe.com>
2021-10-02 14:23:33 -0500

    Verify pipeline in running app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+import pathlib
 from fastapi import FastAPI
 
 app = FastAPI()
 
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+
+MODEL_DIR = BASE_DIR.parent / 'models'
+MODEL_PATH = MODEL_DIR / "exports/spam-sms/spam-model.h5"
 
 @app.get("/")
 def read_index():
     return {
-        "hello": "world"
+        "hello": "world",
+        "models_dir": MODEL_DIR.exists(),
+        "model_path": MODEL_PATH.exists()
     }
